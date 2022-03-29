fd88968e0a118cfd8db6ea77824543f56e196f67
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-03-29 23:44:32 +0300

    дополнили Документ

---
+++ front/pages/_document.jsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(context) {
+    const initialProps = await Document.getInitialProps(context);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ru">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
