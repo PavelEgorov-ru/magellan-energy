b37200b1d427c1698d3d59ab7b88dd4e0995e11f
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-04-11 09:39:55 +0300

    новый файл с кнопкой

---
+++ components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
+import styles from './Htag.module.css';
+
+export const Button = ({ tag, children, colorWith }: ButtonProps): JSX.Element => {
+  return <></>;
+};
