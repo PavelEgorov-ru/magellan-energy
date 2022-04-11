c13415a3d8471cb6a0c0a1aa8554120050f064a6
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-04-11 12:18:51 +0300

    сделан компонент кнопки

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,18 @@
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
-import styles from './Htag.module.css';
+import styles from './Button.module.css';
 
-export const Button = ({ tag, children, colorWith }: ButtonProps): JSX.Element => {
-  return <></>;
+export const Button = ({ view, children, className, ...props }: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={cn(styles.button, className, {
+        [styles.button_simple]: view == 'simple',
+        [styles.button_form]: view == 'form',
+        [styles.button_cards]: view == 'cards',
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import '../styles/global.css';
 import React from 'react';
 import Head from 'next/head';
-import { Htag } from '../components';
+import { Htag, Button } from '../components';
 
-const App = ({ Component, pageProps }) => {
+const App = ({}) => {
+  const Click = (): void => {
+    console.log('Привет кнопка');
+  };
+
   return (
     <>
       <Head>
         <title>Мой первый сайт</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
@@ -25,6 +30,11 @@ const App = ({ Component, pageProps }) => {
       <Htag tag="h4">Самый главный заголовок, который будет на странице</Htag>
       <Htag tag="h5">Самый главный заголовок, который будет на странице</Htag>
       <Htag tag="h6">Самый главный заголовок, который будет на странице</Htag>
+      <Button view="cards" onClick={Click}>
+        Первая кнопка c длинным текстом
+      </Button>
+      <Button view="form">Вторая </Button>
+      <Button view="simple">Третья кнопка</Button>
     </>
   );
 };
